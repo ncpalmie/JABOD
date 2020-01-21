2692e231b09184b504d68c256c41cf16da49b764
Nathan <ncpalmie@calpoly.edu>
2020-01-20 17:58:00 -0800

    Added ability to play audio from bot, added scream command to pull users into private channel for audio

--- JABOD.py
+++ JABOD.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
-import debug, math, random, time, utility
+import debug, math, random, time, utility, asyncio
 
-TOKEN = 'NDM3MzY1MDY5MjI4Mjc3NzYy.XiY9Lw.h8OOAe0kjuDMWmdZ_JaTTlPl2R0'
+TOKEN = 'NDM3MzY1MDY5MjI4Mjc3NzYy.XiZLbA.IG6HqAuHog6FFCYc3M7N3igPLb0'
 client = discord.Client()
 
 #Setup commands to run
@@ -17,6 +17,41 @@ async def execute_commands(client):
     main_guild = utility.get_main_guild(client)
     main_channel = utility.get_main_text_channel(main_guild)
     await debug.parse_commands(client, commands, main_channel)
+
+@client.event
+async def play_audio(audio_file_name, voice_channel):
+    vc = await voice_channel.connect()
+    vc.play(discord.FFmpegPCMAudio(audio_file_name))
+    while vc.is_playing():
+        await asyncio.sleep(1)
+    vc.stop()
+    await vc.disconnect()
+    
+@client.event
+async def on_message(message):
+    author = message.author
+    content = message.content
+    channel = message.channel
+    if content[0] != "$":
+        return
+    #Admin only commands
+    if 'admin' in str(author.top_role):
+        command_args = content[1:].split(' ')
+        #Bounce
+        if 'bounce' in command_args[0] and len(command_args) == 3:
+            if utility.is_member_in_channel(client, command_args[1]):
+                await utility.bounce_member(client, command_args[1], int(command_args[2]))
+            else:
+                await channel.send("That member is not in a voice channel")
+        #Scream Chamber
+        elif 'scream' in command_args[0] and len(command_args) == 2:
+            rand_voice_channel = utility.get_random_voice_channel(client)
+            if utility.is_member_in_channel(client, command_args[1]):
+                member = utility.get_channel_member_by_name(client, command_args[1])
+                await member.edit(voice_channel=rand_voice_channel)
+                await play_audio('scream.mp3', rand_voice_channel)
+            else:
+                await channel.send("That member is not in a voice channel")
 
 @client.event
 async def on_ready():
--- utility.py
+++ utility.py
@@ -10,6 +10,27 @@ def get_random_channel_member(client):
     chosen_member = member_pool[random.randint(0, len(member_pool) - 1)]
     return chosen_member
 
+def get_channel_member_by_name(client, member_name):
+    ret_member = None
+    member_name = member_name.lower()
+    voice_channels = get_main_guild(client).voice_channels
+    for voice_channel in voice_channels:
+        for member in voice_channel.members:
+            if member.name.lower() == member_name:
+                ret_member = member
+    return ret_member
+
+def is_member_in_channel(client, member_name, voice_channel=None):
+    if voice_channel == None:
+        if get_channel_member_by_name(client, member_name) != None:
+            return True
+        return False
+    else:
+        for member in voice_channel.members:
+            if member.name == member_name:
+                return True
+        return False
+
 def get_random_voice_channel(client):
     main_guild = get_main_guild(client)
     return main_guild.voice_channels[random.randint(0, len(main_guild.voice_channels) - 1)]
@@ -24,6 +45,12 @@ def get_main_text_channel(main_guild):
         if text_channel.name == "general":
             return text_channel
 
+def print_in_channel_member_names(client):
+    voice_channels = get_main_guild(client).voice_channels
+    for voice_channel in voice_channels:
+        for member in voice_channel.members:
+            print(member.name)
+
 async def bounce_member(client, member_name, bounces):
     member = None
     if bounces > 10:
@@ -31,6 +58,6 @@ async def bounce_member(client, member_name, bounces):
     if member_name == None:
         member = get_random_channel_member(client)
     else:
-        member = get_member_by_name(member_name)
+        member = get_channel_member_by_name(client, member_name)
     for i in range(0, bounces):
         await member.edit(voice_channel=get_random_voice_channel(client))
